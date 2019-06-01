7ebee5106337c0cc4433bdeae5196ecf74effeb9
Danei lJohnson <danieljohnson2@fastmail.com>
2019-06-01 14:16:43 -0400

    Include vocab in the world model. Add a primitive main loop; you can now move around the map.

--- game.py
+++ game.py
@@ -1,7 +1,44 @@
 class Game():
         def __init__(self, extracted):
-                self.rooms = [Room(x.description) for x in extracted.rooms]
+                self.word_length = extracted.word_length
+                self.rooms = [Room(self, x.description) for x in extracted.rooms]
                 self.player_room = self.rooms[extracted.starting_room]
+
+                self.nouns = dict()
+                for i, g in enumerate(extracted.nouns):
+                        word = Word(g)
+                        for text in g:
+                                self.nouns[normalize_word(text)] = word
+
+                self.north_word = self.get_noun(extracted.nouns[1][0])
+                self.south_word = self.get_noun(extracted.nouns[2][0])
+                self.east_word = self.get_noun(extracted.nouns[3][0])
+                self.west_word = self.get_noun(extracted.nouns[4][0])
+                self.up_word = self.get_noun(extracted.nouns[5][0])
+                self.down_word = self.get_noun(extracted.nouns[6][0])
+                self.directions = [
+                        self.north_word, self.south_word,
+                        self.east_word, self.west_word,
+                        self.up_word, self.down_word
+                ]
+
+                # Hardcoded direction aliases
+                self.nouns["N"] = self.north_word
+                self.nouns["S"] = self.south_word
+                self.nouns["E"] = self.east_word
+                self.nouns["W"] = self.west_word
+                self.nouns["U"] = self.up_word
+                self.nouns["D"] = self.down_word
+
+                self.verbs = dict()
+                for i, g in enumerate(extracted.verbs):
+                        word = Word(g)
+                        for text in g:
+                                self.verbs[normalize_word(text)] = word
+
+                self.go_word = self.get_verb(extracted.verbs[1][0])
+                self.get_word = self.get_verb(extracted.verbs[10][0])
+                self.drop_word = self.get_verb(extracted.verbs[18][0])
 
                 for i, r in enumerate(self.rooms):
                         src = extracted.rooms[i]
@@ -16,8 +53,48 @@ class Game():
                         r.up = resolve_room(src.up)
                         r.down = resolve_room(src.down)
 
+        def is_direction(self, word):
+                return word in self.directions
+
+        def get_noun(self, text):
+                try: return self.nouns[normalize_word(text)]
+                except KeyError: raise KeyError(f"I don't know what '{text}' means.")
+
+        def get_verb(self, text):
+                try: return self.verbs[normalize_word(text)]
+                except KeyError: raise KeyError(f"I don't know what '{text}' means.")
+
+        def parse_command(self, command):
+                parts = command.split()
+                if len(parts) > 2:
+                        raise ValueError("No more than two words!")
+                
+                verb = None
+                noun = None
+
+                if len(parts) >= 1:
+                        try: verb = self.get_verb(parts[0])
+                        except KeyError:
+                                noun = self.get_noun(parts[0])
+                                return (None, noun)
+
+                if len(parts) >= 2:
+                        noun = self.get_noun(parts[1])
+
+                return (verb, noun)
+                
+
+class Word():
+        def __init__(self, aliases):
+                self.text = aliases[0]
+                self.aliases = aliases
+
+        def __str__(self): return self.text
+
 class Room():
-        def __init__(self, description):
+        
+        def __init__(self, game, description):
+                self.game = game
                 self.description = description
                 self.north = None
                 self.south = None
@@ -28,6 +105,17 @@ class Room():
 
         def __repr__(self):
                 return self.description[:32]
+
+        def get_move(self, word):
+                choices = {
+                        self.game.north_word: self.north,
+                        self.game.south_word: self.south,
+                        self.game.east_word: self.east,
+                        self.game.west_word: self.west,
+                        self.game.up_word: self.up,
+                        self.game.down_word: self.down
+                }
+                return choices[word]
 
         def look_text(self):
                 text = self.description
@@ -44,4 +132,10 @@ class Room():
                 if self.up: exits += " Up"
                 if self.down: exits += " Down"
 
-                return text + "\n\nObvious exits:" + exits
+                if exits != "":
+                        text += "\n\nObvious exits:" + exits
+
+                return text
+
+def normalize_word(word):
+        return word[:3].upper()
--- scottdumb.py
+++ scottdumb.py
@@ -8,3 +8,20 @@ ex = ExtractedFile(f)
 g = Game(ex)
 
 print(g.player_room.look_text())
+
+while True:
+        try:
+                cmd = input("What should I do? ")
+                verb, noun = g.parse_command(cmd)
+                print(verb, noun)
+
+                if (verb is None or verb == g.go_word) and g.is_direction(noun):
+                        next = g.player_room.get_move(noun)
+                        if next is None: raise ValueError(f"I can't go there!")
+                        g.player_room = next
+                        print(g.player_room.look_text())
+                else:
+                        raise ValueError("I don't understand.")
+        except Exception as e:
+                print(e)
+
